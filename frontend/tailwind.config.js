4ecc4904384e10aff8e64ccad4ec7cb5e260f962
Abhishek Sharma <abhishek.sharma_cs22@gla.ac.in>
2024-03-03 01:01:53 +0530

    Navbar and Team Section

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
