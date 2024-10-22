fa968a878d68d09bf5650804c1a61228af0a8f84
felixoder <debayanghosh408@gmail.com>
2024-10-22 23:34:59 +0530

    Fixed the allignment of the footer for smaller screens

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,53 +1,36 @@
 import React, { useState } from "react";
-import Sheet from "react-modal-sheet";
-import './style/Navbar.css';
+import "./style/Navbar.css";
+
 export default function Navbar() {
   const [spanStyle, setSpanStyle] = useState({ left: "0px", width: "95px" });
   const widthMapping = [95, 94, 110, 115, 110];
+
   const handleHover = (index) => {
-    const leftPosition = index === 0 ? 0 : widthMapping.slice(0, index).reduce((a, b) => a + b, 0);
-    setSpanStyle({ left: `${leftPosition}px`, width: `${widthMapping[index]}px` });
+    const leftPosition =
+      index === 0 ? 0 : widthMapping.slice(0, index).reduce((a, b) => a + b, 0);
+    setSpanStyle({
+      left: `${leftPosition}px`,
+      width: `${widthMapping[index]}px`,
+    });
   };
+
   return (
-    <div className="relative z-50">
-      <nav className="fixed bottom-5  transform -translate-x-1/2  bg-[#4F46E5] rounded-lg flex justify-center items-center navbar">
-        <a
-          href="#"
-          className="relative inline-block text-white text-base font-medium px-6 z-10 navbar-item"
-          onMouseEnter={() => handleHover(0)}
-        >
-          Home
-        </a>
-        <a
-          href="#"
-          className="relative inline-block text-white text-base font-medium px-6 z-10 navbar-item"
-          onMouseEnter={() => handleHover(1)}
-        >
-          Events
-        </a>
-        <a
-          href="#"
-          className="relative inline-block text-white text-base font-medium px-6 z-10 navbar-item"
-          onMouseEnter={() => handleHover(2)}
-        >
-          Resources
-        </a>
-        <a
-          href="#"
-          className="relative inline-block text-white text-base font-medium px-6 z-10 navbar-item"
-          onMouseEnter={() => handleHover(3)}
-        >
-          Team
-        </a>
-        <a
-          href="#"
-          className="relative inline-block text-white text-base font-medium px-6 z-10 navbar-item"
-          onMouseEnter={() => handleHover(4)}
-        >
-          Join Us
-        </a>
+    <div className="block md:relative z-50">
+      <nav className="fixed bottom-5 transform -translate-x-1/2 bg-[#4F46E5] rounded-lg flex justify-center items-center navbar">
+        {["Home", "Events", "Resources", "Team", "Join"].map(
+          (item, index) => (
+            <a
+              key={index}
+              href="#"
+              className="block md:relative md:inline-block text-white text-base font-sm font-semibold md:font-medium px-2 md:px-6 z-50 navbar-item"
+              onMouseEnter={() => handleHover(index)}
+            >
+              {item}
+            </a>
+          )
+        )}
         <span
-          className="absolute top-0 left-0  rounded-lg transition-all duration-500"
+          className="absolute top-0 left-0 rounded-lg transition-all duration-500"
           style={{ left: spanStyle.left, width: spanStyle.width }}
         />
       </nav>
